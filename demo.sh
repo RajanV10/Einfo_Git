821f3975dce808d4f679d44a9c3a29cfbcf68d78
Rajan10 <rajanvanikar@gmail.com>
2025-07-14 14:53:15 +0530

    Issue solved from hotfix branch

--- demo.sh
+++ demo.sh
@@ -5,3 +5,8 @@ echo "Current date and time: $(date)"
 
 echo "After Creating new branch demo"
 echo "Current date and time: $(date)"
+
+echo "Currently in branch hotfix "
+echo "Current date and time: $(date)"
+echo "Issue is fixed in hotfix branch"
+echo "Now merging hotfix branch into main"
